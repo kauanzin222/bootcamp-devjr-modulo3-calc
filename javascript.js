7003b6ca807872fee5a57eb4475a5aa988b5e31d
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2025-09-12 21:08:47 -0300

    closes #4 - todas as operações da calc

--- javascript.js
+++ javascript.js
@@ -1,10 +1,16 @@
 
 
-function calc() {
+function calc(parameter){
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
+    var result = 0;
     
-    var result = num1 + num2;
+    switch(parameter){
+        case 'soma' : result = num1 + num2; break;
+        case 'sub': result = num1 - num2; break;
+        case 'mult': result = num1 * num2; break;
+        case 'div': result = num1 / num2; break;
+    }
 
     document.getElementById("result").value = result;
 }
