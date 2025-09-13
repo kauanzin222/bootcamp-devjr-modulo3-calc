1dd66c629ca781185d8da423087133a6f9d74472
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2025-09-13 09:20:37 -0300

    implementando o histórico

--- javascript.js
+++ javascript.js
@@ -1,16 +1,20 @@
 
 
-function calc(parameter){
+function calc(op){
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
     var result = 0;
-    
-    switch(parameter){
-        case 'soma' : result = num1 + num2; break;
-        case 'sub': result = num1 - num2; break;
-        case 'mult': result = num1 * num2; break;
-        case 'div': result = num1 / num2; break;
+
+    switch(op){
+        case '+' : result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/': result = num1 / num2; break;
     }
 
     document.getElementById("result").value = result;
+
+    var newHistory = '<div>' + num1 + " " + op + " " + num2 + " " + "= " + result + '</div>';
+    document.getElementById("history").innerHTML = newHistory + document.getElementById("history").innerHTML 
+
 }
